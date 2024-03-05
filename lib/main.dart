9490ce2728f8ff1c793e0c2432a4a2c2d2772770
gilanghaq <gilangkhrismahaq@gmail.com>
2024-03-05 20:20:54 +0700

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_app/screens/pages/getstarted_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Form App',
+      theme: ThemeData(),
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const GetStartedPage(),
+      },
     );
   }
 }
